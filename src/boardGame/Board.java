111fc96d62902a8176407c4792f031111b94d0ef
Carlos Moura <carlos@email.com>
2024-12-10 14:23:04 -0300

    Adicionando exceções

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -6,7 +6,10 @@ public class Board {
     private int columns;
     private final Piece[][] pieces;
 
-    public Board(int columns, int rows) {
+    public Board( int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Error creating Board: there must be at least 1 row and 1 column.");
+        }
         this.columns = columns;
         this.rows = rows;
         pieces = new Piece[rows][columns];
@@ -16,16 +19,8 @@ public class Board {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
     public int getRows() {
         return rows;
-    }
-
-    public void setRows(int rows) {
-        this.rows = rows;
     }
 
     public Piece piece(int row , int column) {
@@ -33,11 +28,32 @@ public class Board {
     }
 
     public Piece piece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Error creating Piece: there is no such position.");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        if (!thereIsAPiece(position)) {
+            throw new BoardException("Error creating Piece: there is already a piece at the position: " + position);
+        }
         pieces[position.getColumn()][position.getRow()] = piece;
         piece.position = position;
+    }
+
+    private boolean positionExsists(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExsists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Error creating Piece: there is no such position.");
+        }
+        return piece(position) != null;
     }
 }
